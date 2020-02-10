c1e03cb53fab7be34fff41e9b4d3eab6ec74e683
Your Name <lukeinuk@gmail.com>
2020-02-10 22:59:09 +0000

    added adguard

--- diy.sh
+++ diy.sh
@@ -8,5 +8,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 'src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
 ./scripts/feeds update -a
 ./scripts/feeds install -a
