ce2eacb3b1af3014d36c3f792f4086eefe42d5f6
downle <lukeinuk@gmail.com>
2020-02-18 21:59:43 +0000

    add luci-app-vssr

--- diy.sh
+++ diy.sh
@@ -9,5 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 'src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 ./scripts/feeds update -a
 ./scripts/feeds install -a
