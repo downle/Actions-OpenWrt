f721149c97811e6df18e24dd46b4bd40b00b83b6
downle <lukeinuk@gmail.com>
2020-02-18 22:07:30 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 'src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+cd lede/package/lean/ 
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
 ./scripts/feeds update -a
 ./scripts/feeds install -a
