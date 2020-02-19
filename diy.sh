132b17bb5a913bfadb4a9a96883bc45ef1f31148
downle <lukeinuk@gmail.com>
2020-02-19 20:06:26 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,11 +8,11 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 'src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
-cd package/lean/ 
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-cd ..
-cd ..
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
+#cd package/lean/ 
+#git clone https://github.com/jerrykuku/lua-maxminddb.git
+#git clone https://github.com/jerrykuku/luci-app-vssr.git
+#cd ..
+#cd ..
 ./scripts/feeds update -a
 ./scripts/feeds install -a
